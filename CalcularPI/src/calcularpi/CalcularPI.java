28f76016d908e2be4aa45993b902276307ed85b9
Yololator <116726856+Yololator@users.noreply.github.com>
2022-12-16 13:51:51 +0100

    v3.-clic derecho refactor, introduce, constant NUM_DIG con modificador de acceso publico.

--- CalcularPI/src/calcularpi/CalcularPI.java
+++ CalcularPI/src/calcularpi/CalcularPI.java
@@ -7,10 +7,10 @@ import java.math.MathContext;
 public class CalcularPI {
 
      public static void main(String[] args) {
-        MathContext mc = new MathContext(10);         
+        MathContext mc = new MathContext(NUM_DIG);   // clic derecho refactor, introduce, constant NUM_DIG con modificador de acceso publico
         //meter en el método compute
         BigDecimal pi = new BigDecimal(0);
-        BigDecimal limit = new BigDecimal(1).movePointLeft(10);
+        BigDecimal limit = new BigDecimal(1).movePointLeft(NUM_DIG);
         boolean stop = false;
         for (int k = 0; !stop; k++) {
             BigDecimal piK = piFunction(k, mc);
@@ -23,6 +23,7 @@ public class CalcularPI {
         System.out.println(pi.round(mc));
         //meter en el método compute
     }
+    public static final int NUM_DIG = 10;
 
     public static BigDecimal piFunction(int k, MathContext mc) {
        //clic derecho, refactor, introduce, method, introduces el nombre y configuras con acceso público
